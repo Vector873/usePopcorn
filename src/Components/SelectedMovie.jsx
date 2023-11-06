d42657ae4a19403beb7f0f1961e822c6c74993f6
Hasan <imtiazhasan873@gmail.com>
2023-11-06 21:34:00 +0500

    fix api

--- src/Components/SelectedMovie.jsx
+++ src/Components/SelectedMovie.jsx
@@ -44,7 +44,7 @@ export default function SelectedMovie({
     async function getMoviesDetails() {
       setisLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setSelectedMovie(data);
